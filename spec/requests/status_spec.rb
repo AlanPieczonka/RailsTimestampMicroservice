052a2e451fcd915f06f6e26cbc7e31169126af01
Alan Pieczonka <alanpieczonka@gmail.com>
2018-01-07 00:20:44 +0100

    Install rspec and write tests for root url

---
+++ spec/requests/status_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe 'Status requests' do 
+    describe 'GET /' do 
+      before { get '/' } 
+
+      it 'returns a json' do 
+        json = JSON.parse(response.body)
+
+        expect(json).not_to be_empty
+        expect(json.size).to eq(2)
+      end
+
+      it 'returns status code 200' do 
+        expect(response).to have_http_status(200)
+      end
+    end
+end
