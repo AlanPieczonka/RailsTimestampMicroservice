9bbf4ee4f459f9d58fee034c48c6ce718b4b3b85
Alan Pieczonka <alanpieczonka@gmail.com>
2018-01-07 00:28:08 +0100

    Refactor application's controller index method by creating Response module that takes object and renders json

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < ActionController::API
+  include Response 
   INFO = {
     info: "Just give me the date after '/' character in the url",
     example: 'localhost:3000/1390283 or localhost:3000/December 15, 2015'    
   }
 
   def index
-    render json: INFO
+    json_response(INFO)
   end
 end
