2ef7428a5eceeac6f8355673350fc86005c4f539
Alan Pieczonka <alanpieczonka@gmail.com>
2018-01-07 18:14:11 +0100

    Add more tests for '/:moment' responses

--- spec/requests/status_spec.rb
+++ spec/requests/status_spec.rb
@@ -16,8 +16,38 @@ RSpec.describe 'Status requests' do
       end
     end
 
-    describe 'GET /:moment' do 
+    describe 'GET /:moment with timestamp' do 
       before { get '/45904853' }
+
+      it 'returns a json' do 
+        json = JSON.parse(response.body)
+
+        expect(json).not_to be_empty
+        expect(json.size).to eq(2)
+      end
+
+      it 'returns status code 200' do 
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    describe 'GET /:moment with date' do 
+      before { get "/December152015" }
+
+      it 'returns a json' do 
+        json = JSON.parse(response.body)
+
+        expect(json).not_to be_empty
+        expect(json.size).to eq(2)
+      end
+
+      it 'returns status code 200' do 
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    describe 'GET /:moment with invalid input' do 
+      before { get "/lkdfjgsdfg" }
 
       it 'returns a json' do 
         json = JSON.parse(response.body)
