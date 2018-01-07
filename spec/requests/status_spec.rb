bb94f808a11459a9e2127e33224b9272e96c31b2
Alan Pieczonka <alanpieczonka@gmail.com>
2018-01-07 17:56:05 +0100

    Add test for '/:moment' route

--- spec/requests/status_spec.rb
+++ spec/requests/status_spec.rb
@@ -15,4 +15,20 @@ RSpec.describe 'Status requests' do
         expect(response).to have_http_status(200)
       end
     end
+
+    describe 'GET /:moment' do 
+      before { get '/45904853' }
+
+      it 'returns a json' do 
+        json = JSON.parse(response.body)
+
+        expect(json).not_to be_empty
+        expect(json.size).to eq(2)
+      end
+
+      it 'returns status code 200' do 
+        expect(response).to have_http_status(200)
+      end
+    end
+
 end
