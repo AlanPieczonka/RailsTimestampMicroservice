f7177455875a79c0b5a99476e1a12ac72eb3061b
Alan Pieczonka <alanpieczonka@gmail.com>
2018-01-07 21:08:17 +0100

    Small naming improvements in application's controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,17 +2,17 @@ class ApplicationController < ActionController::API
   include Response 
   include TimeLogic 
 
-  INFO = {
-    info: "Just give me the date after '/' character in the url",
-    example: 'localhost:3000/1390283 or localhost:3000/December 15, 2015'    
+  INDEX_INFO = {
+    info: "Just give me the timestamp or date after '/' character in the url",
+    example: 'localhost:3000/1450137600 or localhost:3000/December 15, 2015'    
   }
 
   def index
-    json_response(INFO)
+    json_response(INDEX_INFO)
   end
 
   def moment 
-    _results = final_results(params[:moment])
+    _results = results(params[:moment])
     json_response(_results)
   end
 
